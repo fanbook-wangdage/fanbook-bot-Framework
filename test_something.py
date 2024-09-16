04a4e04dac74e539cf3e7e25ca113d6fdd1bfbdb
fanbook-wangdage <124357765+fanbook-wangdage@users.noreply.github.com>
2024-09-16 14:40:57 +0800

    Add files via upload

--- test_something.py
+++ test_something.py
@@ -3,7 +3,7 @@ import subprocess
 def test_example_function():
     try:
         # 30秒不退出视为成功
-        result = subprocess.run('python3 main.py', timeout=30)
+        result = subprocess.run('python main.py', timeout=30)
     except subprocess.TimeoutExpired:
         print("成功")
 
