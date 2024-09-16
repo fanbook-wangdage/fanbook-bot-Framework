5a2af49250b50b7b0074f4e4b0f52d06a56146a6
fanbook-wangdage <124357765+fanbook-wangdage@users.noreply.github.com>
2024-09-16 15:19:45 +0800

    Add files via upload

--- test_something.py
+++ test_something.py
@@ -1,3 +1,8 @@
 import main
 def test_example_function():
-    main.main('test')
+    code=main.main(runtype='test')
+    if code==0:
+        print('test passed')
+    else:
+        #引发错误
+        raise Exception('test failed')
