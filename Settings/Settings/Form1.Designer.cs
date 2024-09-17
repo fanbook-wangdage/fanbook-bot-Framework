aab2f32f5b464cd0d16b518bbc793e75272f3beb
fanbook-wangdage <124357765+fanbook-wangdage@users.noreply.github.com>
2024-09-18 01:57:20 +0800

    Add files via upload

--- Settings/Settings/Form1.Designer.cs
+++ Settings/Settings/Form1.Designer.cs
@@ -36,6 +36,18 @@
             textBox1 = new TextBox();
             label2 = new Label();
             panel2 = new Panel();
+            checkBox4 = new CheckBox();
+            checkBox3 = new CheckBox();
+            label18 = new Label();
+            textBox2 = new TextBox();
+            label17 = new Label();
+            label16 = new Label();
+            label15 = new Label();
+            label14 = new Label();
+            numericUpDown2 = new NumericUpDown();
+            label13 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            label12 = new Label();
             label10 = new Label();
             checkBox2 = new CheckBox();
             label9 = new Label();
@@ -49,6 +61,8 @@
             label11 = new Label();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
             // 
             // listBox1
@@ -136,8 +150,21 @@
             // 
             // panel2
             // 
+            panel2.AutoScroll = true;
             panel2.BackColor = SystemColors.ControlLightLight;
             panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(checkBox4);
+            panel2.Controls.Add(checkBox3);
+            panel2.Controls.Add(label18);
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(label17);
+            panel2.Controls.Add(label16);
+            panel2.Controls.Add(label15);
+            panel2.Controls.Add(label14);
+            panel2.Controls.Add(numericUpDown2);
+            panel2.Controls.Add(label13);
+            panel2.Controls.Add(numericUpDown1);
+            panel2.Controls.Add(label12);
             panel2.Controls.Add(label10);
             panel2.Controls.Add(checkBox2);
             panel2.Controls.Add(label9);
@@ -153,11 +180,118 @@
             panel2.Size = new Size(663, 253);
             panel2.TabIndex = 2;
             // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(125, 230);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(15, 14);
+            checkBox4.TabIndex = 23;
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(374, 92);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(15, 14);
+            checkBox3.TabIndex = 22;
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Location = new Point(329, 90);
+            label18.Name = "label18";
+            label18.Size = new Size(44, 17);
+            label18.TabIndex = 21;
+            label18.Text = "到日志";
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Location = new Point(101, 88);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(222, 23);
+            textBox2.TabIndex = 20;
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Location = new Point(3, 90);
+            label17.Name = "label17";
+            label17.Size = new Size(92, 17);
+            label17.TabIndex = 19;
+            label17.Text = "运行结束时记录";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(3, 228);
+            label16.Name = "label16";
+            label16.Size = new Size(116, 17);
+            label16.TabIndex = 17;
+            label16.Text = "发生异常时写入日志";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(182, 199);
+            label15.Name = "label15";
+            label15.Size = new Size(20, 17);
+            label15.TabIndex = 16;
+            label15.Text = "秒";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(191, 59);
+            label14.Name = "label14";
+            label14.Size = new Size(20, 17);
+            label14.TabIndex = 15;
+            label14.Text = "秒";
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.BorderStyle = BorderStyle.FixedSingle;
+            numericUpDown2.Location = new Point(56, 197);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(120, 23);
+            numericUpDown2.TabIndex = 14;
+            numericUpDown2.Value = new decimal(new int[] { 5, 0, 0, 0 });
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(3, 199);
+            label13.Name = "label13";
+            label13.Size = new Size(56, 17);
+            label13.TabIndex = 13;
+            label13.Text = "重启延时";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.BorderStyle = BorderStyle.FixedSingle;
+            numericUpDown1.Location = new Point(65, 56);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 12;
+            numericUpDown1.Value = new decimal(new int[] { 5, 0, 0, 0 });
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(3, 59);
+            label12.Name = "label12";
+            label12.Size = new Size(56, 17);
+            label12.TabIndex = 10;
+            label12.Text = "重启延时";
+            // 
             // label10
             // 
             label10.AutoSize = true;
             label10.ForeColor = SystemColors.MenuHighlight;
-            label10.Location = new Point(-1, 126);
+            label10.Location = new Point(-4, 245);
             label10.Name = "label10";
             label10.Size = new Size(658, 17);
             label10.TabIndex = 9;
@@ -166,7 +300,7 @@
             // checkBox2
             // 
             checkBox2.AutoSize = true;
-            checkBox2.Location = new Point(145, 109);
+            checkBox2.Location = new Point(149, 170);
             checkBox2.Name = "checkBox2";
             checkBox2.Size = new Size(15, 14);
             checkBox2.TabIndex = 8;
@@ -175,7 +309,7 @@
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(1, 107);
+            label9.Location = new Point(3, 168);
             label9.Name = "label9";
             label9.Size = new Size(140, 17);
             label9.TabIndex = 7;
@@ -185,7 +319,7 @@
             // 
             label8.AutoSize = true;
             label8.ForeColor = SystemColors.MenuHighlight;
-            label8.Location = new Point(-1, 90);
+            label8.Location = new Point(-4, 141);
             label8.Name = "label8";
             label8.Size = new Size(658, 17);
             label8.TabIndex = 6;
@@ -196,7 +330,7 @@
             label7.AutoSize = true;
             label7.Font = new Font("Microsoft YaHei UI", 9F, FontStyle.Bold);
             label7.ForeColor = SystemColors.MenuHighlight;
-            label7.Location = new Point(-1, 73);
+            label7.Location = new Point(3, 124);
             label7.Name = "label7";
             label7.Size = new Size(56, 17);
             label7.TabIndex = 5;
@@ -206,7 +340,7 @@
             // 
             label6.AutoSize = true;
             label6.ForeColor = SystemColors.MenuHighlight;
-            label6.Location = new Point(-1, 56);
+            label6.Location = new Point(-4, 107);
             label6.Name = "label6";
             label6.Size = new Size(658, 17);
             label6.TabIndex = 4;
@@ -227,7 +361,7 @@
             label4.AutoSize = true;
             label4.Font = new Font("Microsoft YaHei UI", 9F, FontStyle.Bold);
             label4.ForeColor = SystemColors.MenuHighlight;
-            label4.Location = new Point(-1, -1);
+            label4.Location = new Point(3, 0);
             label4.Name = "label4";
             label4.Size = new Size(56, 17);
             label4.TabIndex = 2;
@@ -236,7 +370,7 @@
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(155, 39);
+            checkBox1.Location = new Point(161, 36);
             checkBox1.Name = "checkBox1";
             checkBox1.Size = new Size(15, 14);
             checkBox1.TabIndex = 1;
@@ -245,7 +379,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(1, 37);
+            label3.Location = new Point(3, 33);
             label3.Name = "label3";
             label3.Size = new Size(152, 17);
             label3.TabIndex = 0;
@@ -258,7 +392,7 @@
             label11.Name = "label11";
             label11.Size = new Size(25, 17);
             label11.TabIndex = 5;
-            label11.Text = "1.0";
+            label11.Text = "1.1";
             // 
             // Form1
             // 
@@ -280,6 +414,8 @@
             panel1.PerformLayout();
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -305,5 +441,17 @@
         private Label label9;
         private Label label8;
         private Label label11;
+        private Label label12;
+        private NumericUpDown numericUpDown1;
+        private Label label15;
+        private Label label14;
+        private NumericUpDown numericUpDown2;
+        private Label label13;
+        private Label label16;
+        private Label label17;
+        private CheckBox checkBox3;
+        private Label label18;
+        private TextBox textBox2;
+        private CheckBox checkBox4;
     }
 }
--- Settings/Settings/Form1.cs
+++ Settings/Settings/Form1.cs
@@ -12,12 +12,38 @@ namespace Settings
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             listBox1.SelectedIndex = 0;
 
-            string filePath = @"Settigs.json";
+            string filePath = @"Settings.json";
+            try{
+                string json2 = File.ReadAllText(filePath);
+            }catch (FileNotFoundException)
+            {
+                // 如果文件不存在，则创建一个默认的配置文件
+                Person person1 = new Person
+                {
+                    on_start = "",
+                    on_end_restart = false,
+                    on_error_restart = false,
+                    on_end_restart_time=5,
+                    on_err_restart_time=5,
+                    on_end_restart_log="运行完成",
+                    on_end_restart_out_to_file=false,
+                    on_errror_log=true
+                };
+                string json1 = JsonConvert.SerializeObject(person1, Formatting.Indented);
+                File.WriteAllText(filePath, json1);
+            
+            }
             string json = File.ReadAllText(filePath);
             Person person = JsonConvert.DeserializeObject<Person>(json);
             textBox1.Text = person.on_start;
             checkBox1.Checked = person.on_end_restart;
             checkBox2.Checked = person.on_error_restart;
+            numericUpDown1.Value = person.on_end_restart_time;
+            textBox2.Text = person.on_end_restart_log;
+            checkBox3.Checked = person.on_end_restart_out_to_file;
+
+            numericUpDown2.Value = person.on_err_restart_time;
+            checkBox4.Checked = person.on_errror_log;
 
         }
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -51,13 +77,21 @@ namespace Settings
             {
                 on_start = textBox1.Text,
                 on_end_restart = checkBox1.Checked,
-                on_error_restart = checkBox2.Checked
+                on_error_restart = checkBox2.Checked,
+                on_end_restart_time = (int)numericUpDown1.Value,
+                on_err_restart_time = (int)numericUpDown2.Value,
+                on_end_restart_log = textBox2.Text,
+                on_end_restart_out_to_file = checkBox3.Checked,
+                on_errror_log = checkBox4.Checked
+
+                
             };
             string json = JsonConvert.SerializeObject(person, Formatting.Indented);
-            string filePath = @"Settigs.json";
+            string filePath = @"Settings.json";
             File.WriteAllText(filePath, json);
             MessageBox.Show("配置已成功保存", "保存成功");
         }
+
     }
     // 定义一个类来与JSON数据对应
     public class Person
@@ -65,5 +99,10 @@ namespace Settings
         public string on_start { get; set; }
         public bool on_end_restart { get; set; }
         public bool on_error_restart { get; set; }
+        public int on_end_restart_time { get; set; }
+        public int on_err_restart_time { get; set; }
+        public string on_end_restart_log { get; set; }
+        public bool on_end_restart_out_to_file { get; set; }
+        public bool on_errror_log { get; set; }
     }
 }
