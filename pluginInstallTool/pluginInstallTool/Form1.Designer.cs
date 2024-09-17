feae2a5a86b0ca9019d165c69e8dccd15569224d
fanbook-wangdage <124357765+fanbook-wangdage@users.noreply.github.com>
2024-09-17 21:06:00 +0800

    Add files via upload

---
+++ pluginInstallTool/pluginInstallTool/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace pluginInstallTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.BorderStyle = BorderStyle.None;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 17;
+            listBox1.Items.AddRange(new object[] { "test1", "test2" });
+            listBox1.Location = new Point(0, -3);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(135, 204);
+            listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(141, 171);
+            label1.Name = "label1";
+            label1.Size = new Size(152, 17);
+            label1.TabIndex = 1;
+            label1.Text = "在左侧选择一个插件来操作";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(632, 162);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "退出";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(713, 162);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "安装";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft YaHei UI", 10F, FontStyle.Bold);
+            label2.Location = new Point(141, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 19);
+            label2.TabIndex = 4;
+            label2.Text = "插件名";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(141, 48);
+            label3.Name = "label3";
+            label3.Size = new Size(32, 17);
+            label3.TabIndex = 5;
+            label3.Text = "描述";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(800, 197);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "插件安装工具";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Label label2;
+        private Label label3;
+    }
+}
