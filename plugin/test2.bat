b0da46e1e8bec05697fd8ef01f70c278e1f8e7c5
fanbook-wangdage <124357765+fanbook-wangdage@users.noreply.github.com>
2024-09-20 05:23:23 +0800

    添加插件

---
+++ plugin/test2.bat
@@ -0,0 +1,47 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFB1VQgaNAE+/Fb4I5/jH3e+Lp0wOUd4rfZzP5qCyJu0Q73Hreppj02Jf+A==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+chcp 65001
+
+cd plugin
+
+IF EXIST "fb_message_card_editor.exe" (
+    echo File exists.
+    start fb_message_card_editor.exe
+) ELSE (
+    echo 解压后将自动运行，请在弹出的窗口中点击Extract开始解�?    
+    start /wait fmce.exe
+    start fb_message_card_editor.exe
+)
+
