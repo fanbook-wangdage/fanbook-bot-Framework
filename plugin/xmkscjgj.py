5e23688d015e4f998a30c476c513a8e666377dd4
fanbook-wangdage <124357765+fanbook-wangdage@users.noreply.github.com>
2024-09-20 03:44:34 +0800

    Add files via upload

--- plugin/xmkscjgj.py
+++ plugin/xmkscjgj.py
@@ -4,18 +4,26 @@ file_name = input('请输入项目名称：')
 token=input('请输入bot的token：')
 botid=input('请输入bot的长id：')
 code="token='"+token+"""'
+from fanbookbotapi import *
+import json
+
+# 本段代码由工具自动生成，是一个将你的消息重新发出去的示例，你可以直接运行
+# 你可以自由更改onMessage函数里面的代码，但是我建议你保留解析消息的那一部分
+# onOpen、onMessage、onError、onClose分别用于连接建立时触发、收到消息时触发、出错时触发、连接关闭时触发，可以使用ws参数来完成一些操作，但是你一般不需要使用这个，发送心跳包将由库自动实现
+# 强烈建议在处理消息部分添加try或者在配置编辑器内设置出错时自动重启，onMessage出错时很有可能断开连接并导致程序意外终止
+
 def onOpen(ws):
     pass
 def onMessage(ws, message):
     global token
-    message=json.loads(message)#解析json为字典
-    if message["action"] =="push":#如果是推送
-        if message["data"]["author"]["bot"] == False:#忽略bot消息
-            content = json.loads(message["data"]["content"])#这里获取消息内容"""+'\n            if "${@!'+botid+'}" in content["text"]:#筛选@这个机器人的消息，注意这里的botid改为你机器人的长id'+"""\n                #我们将逻辑写在这里
-                guild_id=message["data"]["guild_id"]#获取消息来源服务器id
-                channel_id=message["data"]["channel_id"]#获取消息来源频道id
-                text=content['text'][23:]#去掉@以后的内容
-                #发送消息
+    message=json.loads(message)# 解析json为字典
+    if message["action"] =="push":# 如果是推送
+        if message["data"]["author"]["bot"] == False:# 忽略bot消息
+            content = json.loads(message["data"]["content"])# 这里获取消息内容"""+'\n            if "${@!'+botid+'}" in content["text"]:#筛选@这个机器人的消息，注意这里的botid改为你机器人的长id'+"""\n                # 我们将逻辑写在这里
+                guild_id=message["data"]["guild_id"]# 获取消息来源服务器id
+                channel_id=message["data"]["channel_id"]# 获取消息来源频道id
+                text=content['text'][23:]# 去掉@以后的内容
+                # 发送消息
                 sendmessage(token,chatid=int(channel_id),type='text',text=text)
             
 def onError(ws, error):
